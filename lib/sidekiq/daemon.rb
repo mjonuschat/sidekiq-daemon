9b7213d9d51ada4a01a6c591716128f5588d8296
Morton Jonuschat <yabawock@gmail.com>
2013-09-10 18:35:16 +0200

    Initial commit

---
+++ lib/sidekiq/daemon.rb
@@ -0,0 +1,7 @@
+require "sidekiq/daemon/version"
+
+module Sidekiq
+  module Daemon
+    # Your code goes here...
+  end
+end
---
+++ lib/sidekiq/daemon/version.rb
@@ -0,0 +1,5 @@
+module Sidekiq
+  module Daemon
+    VERSION = "0.0.0"
+  end
+end
